6ff0b39042ee80cfd01ba2caa801dfd48fa17355
EdwinhoAG <115709979+EdwinhoAG@users.noreply.github.com>
2023-02-06 13:54:59 +0100

    Update dependencies.php

--- INCOM_PD_EDWIN/views/dependencies.php
+++ INCOM_PD_EDWIN/views/dependencies.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" type="text/css" href="../bookstore/alertifyjs/css/alertify.css">
 <link rel="stylesheet" type="text/css" href="../bookstore/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" type="text/css" href="../bookstore/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="../bookstore/select2/dist/css/select2.css">
 <link rel="stylesheet" type="text/css" href="../css/btns.css">
 <link rel="stylesheet" type="text/css" href="../css/footer.css">
 
@@ -13,5 +12,4 @@
 <script src="../bookstore/jquery-3.6.3.min.js"></script>
 <script src="../bookstore/alertifyjs/alertify.js"></script>
 <script src="../bookstore/bootstrap/js/bootstrap.js"></script>
-<script src="../bookstore/select2/dist/js/select2.js"></script>
 <script src="../js/functions.js"></script>
